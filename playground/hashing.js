80e68638342693089d98682b73b25ae2a309b8bf
Trideep Lal Das <trideeplaldas@hotmail.com>
2019-02-21 02:05:42 +1100

    Hashed the password before it's saved in database

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+/**To hash a password using bcrypt we need to call 2 methods */
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$9aDPBlL75zac23JWkw84B.b7GZdGdkLDdN/Ht/pGQ27g5Pf50tOdq';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // var message = 'We are number one';
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -74,6 +75,24 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.pre('save', function (next) {
+    var user = this;
+    //checking down below if pass is verified or changed i forgot
+    if (user.isModified('password')) {
+        
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User}
+
+//**Btw we're using regular functions here to gain access to the this binding */
