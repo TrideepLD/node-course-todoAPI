2942ed41d8e949bd04b3bb189374459a904121a5
Trideep Lal Das <trideeplaldas@hotmail.com>
2019-02-22 01:19:15 +1100

    Added the first describe it file that made me find out about the error

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
 beforeEach(populateUsers);
 beforeEach(populateTodos);
@@ -182,5 +183,19 @@ describe('PATCH /todos/:id', () => {
       expect(res.body.todo.completedAt).toBeNull();
     })
     .end(done);
+  });
+});
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
   });
 });
