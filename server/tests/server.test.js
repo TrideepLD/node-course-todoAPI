271c137bee0ceb7bd219a7acb174391e9f7f2ef3
Trideep Lal Das <trideeplaldas@hotmail.com>
2019-03-04 01:09:45 +1100

    Added Test case for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -319,3 +319,26 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    // DELETE /users/me/token
+    //Set x-auth equal to token
+    //expect 200
+    //Find user, verify that tokens array has length of zero
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+    User.findById(users[1]._id).then((user) => {
+      expect(user.tokens.length).toBe(0);
+      done();
+    }).catch((e) => done(e));
+  });
+});
+});
